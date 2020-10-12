5b847428d6f7940a26e3f1b0f9b184720db3fd44
zaraayvazyan9 <zara.ayvazyan9@gmail.com>
2020-10-12 14:08:05 +0200

    changed category search cards stylings add search API

--- script.js
+++ script.js
@@ -7,3 +7,31 @@ function burgermenu() {
     x.style.display = "block";
   }
 }
+document.addEventListener("submit", burgermenu);
+
+// Search function
+// const API_Key = "8e4669c252cd8c46bd10e12b16db058a";
+
+
+function handleSubmit(e) {
+  let text = document.getElementById("searchText").value;
+  let imgElem = document.getElementById("imgElement");
+  let query =
+    'https://api.edamam.com/search?q=${text}&app_id=225b43f6&app_key=8ca7eadc17dbccc23e301761ab625c3e&from=0&to=3';
+
+  // do a search
+  fetch(query)
+    .then((response) => response.json())
+    .then((data) {
+      console.log(data);
+      let url = data.hits[0].recipe.image;
+      console.log(url);
+      imgElem.src = url;
+    });
+
+  //put the image in the div
+  
+  imgElem.src =
+    "https://www.edamam.com/web-img/e42/e42f9119813e890af34c259785ae1cfb.jpg";
+}
+document.getElementById("searchButton").addEventListener("click", handleSubmit);
